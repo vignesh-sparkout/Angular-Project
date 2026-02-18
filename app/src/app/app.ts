4af14ebbce677f3e58e08d9ecd2515ee0bf082d6
vignesh-sparkout <vignesh@sparkouttech.com>
2026-02-18 13:06:37 +0530

    Project Modify

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBar } from './shared/side-bar/side-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,        
-  imports: [RouterOutlet],  // ✅ Add comma
+  imports: [RouterOutlet, SideBar],  
   template: `
     <router-outlet></router-outlet>
   `
